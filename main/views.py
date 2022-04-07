d5596640a7dd295a3dab5c87bf4317a356316964
0seoYun <yys020819@gmail.com>
2022-04-07 14:15:29 +0900

    2주차과제 Django자기소개

--- main/views.py
+++ main/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def showmain(request):
     return render(request, 'main/mainpage.html')
-    
+
+def introduction(request):
+    return render(request, 'main/show.html')
