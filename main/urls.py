e40e659473fff926cc13c8ee93fce05644ff4e27
0seoYun <yys020819@gmail.com>
2022-05-15 20:07:32 +0900

    5th HW(1)

---
+++ main/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import *
+
+app_name = "main"
+urlpatterns = [
+    path('', showmain, name="showmain"),
+
+    path('introduction/', introduction, name='introduction'),
+
+    path('firstpage/', showfirst, name="showfirst"),
+
+    path('secondpage/', showsecond, name="showsecond"),
+
+    path('<str:id>',detail, name="detail"),
+
+    path('new/',new, name="new"),
+
+    path('create/',create, name="create"),
+
+    path('edit/<int:id>', edit, name="edit"),
+
+    path('update/<int:id>', update, name="update"),
+
+    path('delete/<int:id>', delete, name="delete"),
+]
