3a6a33a7046bb2910c033bc47a6466eee2e2f927
0seoYun <yys020819@gmail.com>
2022-05-24 18:25:21 +0900

    6th HW (2)

--- sns_project/settings.py
+++ sns_project/settings.py
@@ -138,6 +138,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
